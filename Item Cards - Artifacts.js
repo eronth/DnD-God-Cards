e03fac0f0c6774e6fb7788aa6f13bb42833fa9cb
eronth <eronth@gmail.com>
2014-12-15 07:06:22 -0600

    Added first card and cards to be done.

---
+++ Item Cards - Artifacts.js
@@ -0,0 +1,33 @@
+Druid Armor
+
+Mighty Sword
+
+Gold? Shadow Mask
+
+Shadow Gloves
+
+Magic Broach
+
+Cursed Ring
+
+Cursed Gauntlets
+
+
+{
+    "count": 1,
+    "color": "dimgray",
+    "title": "Full Plate",
+    "icon": "custom-armor-heavy",
+    "contents": [
+        "subtitle | Heavy armor (1500gp)",
+        "rule",
+        "property | AC | 18",
+        "property | Strength required | 15",
+        "property | Stealth | Disadvantage",
+        "rule",
+        "fill | 2",
+        "description | Heavy | Your speed is reduced by 10 feet.",
+        "description | Stealth | You have disadvantage on Stealth.",
+        "fill | 3"
+    ]
+}
