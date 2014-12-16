e724c068a63080b4920caa0404c9e4967230610f
eronth <eronth@gmail.com>
2014-12-16 10:37:13 -0600

    Added brooch. fixed more commas.

--- Item Cards - Artifacts.js
+++ Item Cards - Artifacts.js
@@ -1,18 +1,33 @@
 Druid Armor
 Gold(?) Shadow Mask
 Shadow Gloves
-Magic Broach
 Cursed Ring
 Cursed Gauntlets
 
+
 <  5  =  Rising Hero    
-
 > 10  =  Heroic
-
 > 15  =  Epic
-
   20  =  Legendary
 
+
+{
+  "count": 1,
+  "color": "gold",
+  "title": "Brooch of Shielding",
+  "icon": "artifact",
+  "contents": [
+    "subtitle | Golden protective brooch",
+    "rule",
+    "property | Weight | 1 lbs",
+    "property | Attributes | Requires attunement"
+    "rule",
+    "fill | 2",
+    "text | A thin golden brooch with a little bit of decorative embelleshment.",
+    "text | You have resistance to force damage."
+    "text | You have immunity to magic missile damage.",
+  ]
+}
 {
   "count": 1,
   "color": "lightning",
@@ -26,9 +41,9 @@ Cursed Gauntlets
     "property | Attributes | Finesse, light"
     "rule",
     "fill | 2",
-    "text | A shortsword with a bluish tint to it. Swinging it leaves a short trail of blue sparks and electricity."
-    "text | Whenever you damage a creature with this weapon, choose a creature within 5 feet not already being damaged by your attack. Use Arcing Bolt on the chosen creature as an extension of the current attack." 
-    "text | This attack does 1d6 Lightning damage."
+    "text | A shortsword with a bluish tint to it. Swinging it leaves a short trail of blue sparks and electricity.",
+    "text | Whenever you damage a creature with this weapon, choose a creature within 5 feet not already being damaged by your attack. Use Arcing Bolt on the chosen creature as an extension of the current attack.", 
+    "text | This attack does 1d6 Lightning damage.",
     "section | Arcing Bolt",
     "subtitle | Attack",
     "property | Damage | 1d4 Lightning",
