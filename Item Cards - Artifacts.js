88494621650bf9818984b9ca6c30f58ffda7cfa5
eronth <eronth@gmail.com>
2014-12-16 09:09:58 -0600

    Horrible spacing fixed.

--- Item Cards - Artifacts.js
+++ Item Cards - Artifacts.js
@@ -14,28 +14,28 @@ Cursed Gauntlets
   20  =  Legendary
 
 {
-    "count": 1,
-    "color": "lightning",
-    "title": "Mighty Sword",
-    "icon": "sword",
-    "contents": [
-        "subtitle | Shocking Short sword",
-        "rule",
-        "property | Damage | 1d6 Lighting",
-        "property | Weight | 3 lbs",
-        "property | Attributes | Finesse, light"
-        "rule",
-        "fill | 2",
-        "text | A shortsword with a bluish tint to it. Swinging it leaves a short trail of blue sparks and electricity."
-        "text | Whenever you damage a creature with this weapon, choose a creature within 5 feet not already being damaged by your attack. Use Arcing Bolt on the chosen creature as an extension of the current attack." 
-        "text | This attack does 1d6 Lightning damage."
-        "section | Arcing Bolt",
-        "subtitle | Attack",
-        "property | Damage | 1d4 Lightning",
-        "text | Roll a 1d20 and add your proficiency for the attack roll. No other modifiers are added to this attack.",
-        "text | This attack does 1d6 Lightning damage.",
-        "text | <i>Damage dealt by Arcing Bolt triggers another instance of Arcing Bolt.</i>",
-        "section | Epic Weapon",
-        "text | If your character is level 11 or higher, trigger Arcing Bolt a second time whenever you deal damage."
-    ]
+  "count": 1,
+  "color": "lightning",
+  "title": "Mighty Sword",
+  "icon": "sword",
+  "contents": [
+    "subtitle | Shocking Short sword",
+    "rule",
+    "property | Damage | 1d6 Lighting",
+    "property | Weight | 3 lbs",
+    "property | Attributes | Finesse, light"
+    "rule",
+    "fill | 2",
+    "text | A shortsword with a bluish tint to it. Swinging it leaves a short trail of blue sparks and electricity."
+    "text | Whenever you damage a creature with this weapon, choose a creature within 5 feet not already being damaged by your attack. Use Arcing Bolt on the chosen creature as an extension of the current attack." 
+    "text | This attack does 1d6 Lightning damage."
+    "section | Arcing Bolt",
+    "subtitle | Attack",
+    "property | Damage | 1d4 Lightning",
+    "text | Roll a 1d20 and add your proficiency for the attack roll. No other modifiers are added to this attack.",
+    "text | This attack does 1d6 Lightning damage.",
+    "text | <i>Damage dealt by Arcing Bolt triggers another instance of Arcing Bolt.</i>",
+    "section | Epic Weapon",
+    "text | If your character is level 11 or higher, trigger Arcing Bolt a second time whenever you deal damage."
+  ]
 }
