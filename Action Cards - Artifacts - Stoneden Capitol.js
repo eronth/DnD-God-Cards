7df8f455bf2f61dbf44a4cb9add3af1143034202
eronth <eronth@gmail.com>
2014-12-19 17:46:00 -0600

    Update and rename Action Cards - Artifacts - Stoneden Capitol.js to Item Cards - Artifacts - Stoneden Capitol.js

--- Action Cards - Artifacts - Stoneden Capitol.js
+++
@@ -1,6 +0,0 @@
-The Eye of Blood Diamonds
-*Cloak of jimpity jump jamp.
-*Blood Talon - Deal (1d4)d(1d4+1) damage.
-*Helm of Red Dragons
-*Red Dragon Scale Armor
-*Blade of the Fire Whip
---
+++ Item Cards - Artifacts - Stoneden Capitol.js
@@ -0,0 +1,22 @@
+The Eye of Blood Diamonds
+*Cloak of jimpity jump jamp.
+*Blood Talon - Deal (1d4)d(1d4+1) damage.
+*Helm of Red Dragons
+*Red Dragon Scale Armor
+*Blade of the Fire Whip
+{
+"count": 1,
+"color": "darkgrey",
+"title": "Resetti Stone",
+"icon": "equipment",
+"contents": [
+"subtitle | Mask of Shadows",
+"rule",
+"Property | Charges | 1 max",
+"property | Weight | 1 lbs",
+"rule",
+"fill | 2",
+"text | You may mark a moment in time. For the next 24 hours, you can choose to revert to that time. You retain all memories of what you did before you reverted.",
+"text | You may use a charge to extend a mark by 92 hours. You regain 1 charge every 6 weeks."
+]
+}
