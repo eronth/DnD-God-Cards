1244b97900ebfca0dedc1338a7977946b7bf64f2
eronth <eronth@gmail.com>
2014-12-19 17:53:51 -0600

    Update Item Cards - Artifacts - Stoneden Capitol.js

--- Item Cards - Artifacts - Stoneden Capitol.js
+++ Item Cards - Artifacts - Stoneden Capitol.js
@@ -1,22 +1,24 @@
 The Eye of Blood Diamonds
-*Cloak of jimpity jump jamp.
-*Blood Talon - Deal (1d4)d(1d4+1) damage.
-*Helm of Red Dragons
-*Red Dragon Scale Armor
-*Blade of the Fire Whip
+Cloak of jimpity jump jamp.
+Blood Talon - Deal (1d4)d(1d4+1) damage.
+
+Helm of Red Dragons
+Red Dragon Scale Armor
+
 {
-"count": 1,
-"color": "darkgrey",
-"title": "Resetti Stone",
-"icon": "equipment",
-"contents": [
-"subtitle | Mask of Shadows",
-"rule",
-"Property | Charges | 1 max",
-"property | Weight | 1 lbs",
-"rule",
-"fill | 2",
-"text | You may mark a moment in time. For the next 24 hours, you can choose to revert to that time. You retain all memories of what you did before you reverted.",
-"text | You may use a charge to extend a mark by 92 hours. You regain 1 charge every 6 weeks."
-]
+  "count": 1,
+  "color": "red",
+  "title": "Blade of the Fire Whip",
+  "icon": "sword",
+  "contents": [
+    "subtitle | A fiery blade",
+    "rule",
+    "Property | Attributes | Versatile (1d10)",
+    "Property | Damage | 1d8 fire",
+    "property | Weight | 3 lbs",
+    "rule",
+    "fill | 2",
+    "text | A flaming sword that can transform into a whip.",
+    "text | You can use an action to extend the fire from this blade into a whip that deals <b><1d4</b> damage and has <b>15 foot reach</b>. You have disadvantage on all attacks with this weapon in a 5 foot range until you use an action to retract the whip."
+  ]
 }
