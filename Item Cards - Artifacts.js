982f78a4187e87b7ff6c20a14a82e050bada173d
eronth <eronth@gmail.com>
2014-12-19 17:39:25 -0600

    Update Item Cards - Artifacts.js

--- Item Cards - Artifacts.js
+++ Item Cards - Artifacts.js
@@ -14,7 +14,22 @@ Tsuni''s new box.
 > 10  =  Heroic
 > 15  =  Epic
   20  =  Legendary
-Gold(?) Shadow Mask
+{
+  "count": 1,
+  "color": "darkgrey",
+  "title": "Resetti Stone",
+  "icon": "equipment",
+  "contents": [
+    "subtitle | Mask of Shadows",
+    "rule",
+    "Property | Charges | 1 max",
+    "property | Weight | 1 lbs",
+    "rule",
+    "fill | 2",
+    "text | You may mark a moment in time. For the next 24 hours, you can choose to revert to that time. You retain all memories of what you did before you reverted.",
+    "text | You may use a charge to extend a mark by 92 hours. You regain 1 charge every 6 weeks."
+  ]
+}
 {
   "count": 1,
   "color": "darkgrey",
